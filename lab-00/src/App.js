70d0c32c83558905666214614ee4412005897887
Joao Neves <joao.m.guedes.neves@avanade.com>
2022-04-28 18:40:06 +0200

    add lab 00

---
+++ lab-00/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Lab 00</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ lab-01-begin/src/index.js
@@ -0,0 +1,11 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+ <title>lab-01</title>
+</head>
+<body>
+ <div id="app"></div>
+</body>
+</html>
