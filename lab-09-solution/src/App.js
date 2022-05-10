69f2657b1497a439ce3d58c66f0c8acb004c7cf7
micasno <micasno@gmail.com>
2022-05-10 12:42:30 +0200

    add lab9

---
+++ lab-09-solution/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import Home from "./Home";
+import Menu from "./Menu";
+export default function App() {
+  return (
+    <>
+      <Menu />
+    </>
+  );
+}
