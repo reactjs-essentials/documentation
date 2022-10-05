528edf80f8045ecf72297c1e2e5096c22f82eb36
Miguel Castillo Nolasco <m.nolasco.castillo@avanade.com>
2022-10-05 09:32:48 +0200

    update

--- lab-context-solution/src/context/SharedContext.js
+++ lab-context-solution/src/context/SharedContext.js
@@ -3,7 +3,7 @@ import React , {useState} from 'react'
 const SharedContext = React.createContext({})
 
 export function SharedContextProvider({children}){
-    const [info, setInfo] = useState(['No data info')
+    const [info, setInfo] = useState(['No data info'])
     return (
         <SharedContext.Provider value ={{info, setInfo}}>
             {children}
